b68945aa667768e0aba92ad7e51a913cbee6a2f0
Craig Minoff <craigminoff@gmail.com>
2014-06-19 00:33:20 -0400

    Updated Calculator

--- tip_calculator1.rb
+++ tip_calculator1.rb
@@ -1,4 +1,20 @@
-The pre-tax cost of your meal was $20.00
-At 12%, tax for this meal is $2.40.
-For a 20% tip, you should leave $4.48.
-The grand total for this meal is then $26.88.
+#print "What did the meal cost"
+meal_cost = 20
+meal_cost.to_f
+#print "What is the tax rate (%)?"
+tax_rate_percent = 12
+tax_rate = tax_rate_percent/100.to_f
+tax_rate.to_f
+#print "What do you tip (%)?"
+tip_rate_percent = 20
+tip_rate = tip_rate_percent/100.to_f
+tip_rate.to_f
+
+tax = tax_rate * meal_cost
+tip = tip_rate * meal_cost
+total = tax + tip + meal_cost
+
+puts "The pre-tax cost of your meal was $#{meal_cost}"
+puts "At #{tax_rate_percent}%, tax for this meal is $#{tax}."
+puts "For a #{tip_rate_percent}% tip, you should leave $#{tip}."
+puts "The grand total for this meal is then $#{total}."
