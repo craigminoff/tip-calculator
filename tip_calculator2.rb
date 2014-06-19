fb299c545b1960ecd81a416b4c57a1b16303a586
Craig Minoff <craigminoff@gmail.com>
2014-06-19 00:58:17 -0400

    Added second calculator and solution

--- tip_calculator2.rb
+++ tip_calculator2.rb
@@ -1,15 +1,15 @@
-print "What did the meal cost"
-meal_cost = gets.chomp
-print "What is the tax rate (%)?"
-tax_rate = gets.chomp
-print "What do you tip (%)?"
-tip_rate = gets.chomp
+puts "What did the meal cost"
+meal_cost = gets.chomp.to_f
+puts "What is the tax rate (%)?"
+tax_rate = gets.chomp.to_f
+puts "What do you tip (%)?"
+tip_rate = gets.chomp.to_f
 
 tax = tax_rate/100 * meal_cost
 tip = tip_rate/100 * meal_cost
 total = tax + tip + meal_cost
 
-puts "The pre-tax cost of your meal was $%.2f.\n" % meal_cost
-puts "At %d%%, tax for this meal is $%.2f.\n" % [tax_rate, tax]
-puts "For a %d%% tip, you should leave $%.2f.\n" % [tip_rate, tip]
-puts "The grand total for this meal is then $%.2f.\n" % total
+print "The pre-tax cost of your meal was $%.2f.\n" % meal_cost
+print "At %d%%, tax for this meal is $%.2f.\n" % [tax_rate, tax]
+print "For a %d%% tip, you should leave $%.2f.\n" % [tip_rate, tip]
+print "The grand total for this meal is then $%.2f.\n" % total
