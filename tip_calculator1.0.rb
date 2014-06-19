d73cb77ae2edb51c7e0d389c662a07a7a2d6d67c
Craig Minoff <craigminoff@gmail.com>
2014-06-19 00:48:47 -0400

    Added solution and updated answer

---
+++ tip_calculator1.0.rb
@@ -0,0 +1,15 @@
+#print "What did the meal cost"
+meal_cost = 20
+#print "What is the tax rate (%)?"
+tax_rate = 12
+#print "What do you tip (%)?"
+tip_rate = 20
+
+tax = meal_cost * tax_rate/100
+tip = meal_cost * tip_rate/100
+total = tax + tip + meal_cost
+
+puts "The pre-tax cost of your meal was $%.2f.\n" % meal_cost
+puts "At %d%%, tax for this meal is $%.2f.\n" % [tax_rate, tax]
+puts "For a %d%% tip, you should leave $%.2f.\n" % [tip_rate, tip]
+puts "The grand total for this meal is then $%.2f.\n" % total
